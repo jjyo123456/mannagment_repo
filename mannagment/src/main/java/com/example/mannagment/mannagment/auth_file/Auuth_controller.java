d229da0859ea15cb67e2dd52a5f941c1fe7ed88a
jjyo123456 <jasjyotsingh11@gmail.com>
2025-05-03 21:51:30 +0530

    exception added

--- mannagment/src/main/java/com/example/mannagment/mannagment/auth_file/Auuth_controller.java
+++ mannagment/src/main/java/com/example/mannagment/mannagment/auth_file/Auuth_controller.java
@@ -1,25 +1,50 @@
 package com.example.mannagment.mannagment.auth_file;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController("auth/")
+@RestController
+@RequestMapping("/auth")
 public class Auuth_controller {
+Auth_repository repository;
 
-    Auth_repository repository;
+@Autowired
+public Auuth_controller(Auth_repository repository) {
+    this.repository = repository;
+}
 
-    @Autowired
-    public Auuth_controller(Auth_repository repository) {
-        this.repository = repository;
+@ExceptionHandler(ErrorException.class)
+public ResponseEntity<Erroe_response_object> exceptionhandler(ErrorException error){
+    Erroe_response_object error_response_object = new Erroe_response_object();
+    error_response_object.setMessage(error.getMessage());
+    error_response_object.setStatus("hehehehehe");
+    error_response_object.setTimestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+    return new ResponseEntity<>(error_response_object, HttpStatus.NOT_FOUND);
+}
+
+@PostMapping("/signup")
+public void sign_up(@RequestBody user_object new_userr){
+    this.repository.save(new_userr);
+}
+
+@GetMapping("/login/{name}")
+public user_object login_method(@PathVariable("name") String name){
+    user_object found = this.repository.findbyname(name);
+    if (found == null) {
+        throw new ErrorException("User not found");
     }
-    @RequestMapping("signup/{new_user}")
-    public void sign_up(@PathVariable("new_user") user_object new_userr){
-        this.repository.save(new_userr);
-    }
-    @RequestMapping("login/{name}")
-    public user_object login_method(@PathVariable("name") String name){
-        return this.repository.findbyname(name);
-    }
+    return found;
+}
 }
