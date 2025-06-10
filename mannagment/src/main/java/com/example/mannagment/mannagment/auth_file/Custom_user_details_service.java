998c26e06b7476a69e99c62c3a6abe6dc7ec3bdc
jjyo123456 <jasjyotsingh11@gmail.com>
2025-06-10 16:39:47 +0530

    added user details service class custom loadbyusername method

---
+++ mannagment/src/main/java/com/example/mannagment/mannagment/auth_file/Custom_user_details_service.java
@@ -0,0 +1,22 @@
+package com.example.mannagment.mannagment.auth_file;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class Custom_user_details_service implements UserDetailsService{
+    @Autowired
+    Auth_repository auth_repository;
+    
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        user_object user = auth_repository.findbyname(username);
+
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), new ArrayList<>());
+    }
+}
