b0b41e7d73ddf11ebc3bcf4f9960b775584e1b0e
jjyo123456 <jasjyotsingh11@gmail.com>
2025-08-25 03:41:23 +0530

    small commit

---
+++ mannagment/src/main/java/com/example/mannagment/mannagment/sos_file/ambulance_details/Ambulance_details_service.java
@@ -0,0 +1,8 @@
+package com.example.mannagment.mannagment.sos_file.ambulance_details;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Ambulance_details_service {
+
+}
--- mannagment/src/main/java/com/example/mannagment/mannagment/sos_file/ambulance_details/Hopsital_details_repo.java
+++ mannagment/src/main/java/com/example/mannagment/mannagment/sos_file/ambulance_details/Hopsital_details_repo.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Hopsital_details_repo extends JpaRepository<Hospital_data_class , > {
+public interface Hopsital_details_repo extends JpaRepository<Hospital_data_class , Long> {
 }
